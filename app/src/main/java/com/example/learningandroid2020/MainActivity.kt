7bf744f45635da64518c1db8128a3d173ce4d0e4
TiepPt <tieppt07@gmail.com>
2020-05-24 23:57:05 +0700

    OkHttpClient

--- app/src/main/java/com/example/learningandroid2020/MainActivity.kt
+++ app/src/main/java/com/example/learningandroid2020/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.learningandroid2020
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import com.squareup.okhttp.Response
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://api.github.com/search/repositories?q=tiep&sort=stars&order=desc")
+            .build()
+
+        val response: Response = client.newCall(request).execute()
+
+        Log.d("response", response.body().string());
     }
 }
+
