f9bc01882eac6e375de2e105601b90ee31886184
TiepPt <tieppt07@gmail.com>
2020-05-28 17:00:11 +0700

    request API

--- app/src/main/java/com/example/learningandroid2020/MainActivity.kt
+++ app/src/main/java/com/example/learningandroid2020/MainActivity.kt
@@ -3,116 +3,39 @@ package com.example.learningandroid2020
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
-//import android.support.v7.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
-
-    val CONNECTON_TIMEOUT_MILLISECONDS = 60000
+    val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btGetInfo.setOnClickListener {
-            var city = edtCityName.text.toString()
-            val url =
-                "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + city + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-            GetWeatherAsyncTask().execute(url)
-        }
-    }
+        object: AsyncTask<Void, Void, String?>() {
 
-    inner class GetWeatherAsyncTask : AsyncTask<String, String, String>() {
-
-        override fun onPreExecute() {
-            // Before doInBackground
-        }
-
-        override fun doInBackground(vararg urls: String?): String {
-            var urlConnection: HttpURLConnection? = null
-
-            try {
-                val url = URL(urls[0])
-
-                urlConnection = url.openConnection() as HttpURLConnection
-                urlConnection.connectTimeout = CONNECTON_TIMEOUT_MILLISECONDS
-                urlConnection.readTimeout = CONNECTON_TIMEOUT_MILLISECONDS
-
-                var inString = streamToString(urlConnection.inputStream)
-                Log.i("string", inString);
-
-                publishProgress(inString)
-            } catch (ex: Exception) {
-
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect()
-                }
+            override fun doInBackground(vararg params: Void?): String? {
+                return getContent()
             }
 
-            return " "
-        }
-
-        override fun onProgressUpdate(vararg values: String?) {
-            try {
-                var json = JSONObject(values[0])
-
-                val query = json.getJSONObject("query")
-                val results = query.getJSONObject("results")
-                val channel = results.getJSONObject("channel")
-
-                val location = channel.getJSONObject("location")
-                val city = location.get("city")
-                val country = location.get("country")
-
-                val humidity = channel.getJSONObject("atmosphere").get("humidity")
-
-                val condition = channel.getJSONObject("item").getJSONObject("condition")
-                val temp = condition.get("temp")
-                val text = condition.get("text")
-
-                tvWeatherInfo.text =
-                    "Location: " + city + " - " + country + "\n" +
-                            "Humidity: " + humidity + "\n" +
-                            "Temperature: " + temp + "\n" +
-                            "Status: " + text
-
-            } catch (ex: Exception) {
-
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                println("-------------------- result: $result")
             }
-        }
 
-        override fun onPostExecute(result: String?) {
-            // Done
-        }
+        }.execute()
     }
 
-    fun streamToString(inputStream: InputStream): String {
-
-        val bufferReader = BufferedReader(InputStreamReader(inputStream))
-        var line: String
-        var result = ""
-
-        try {
-            do {
-                line = bufferReader.readLine()
-                Log.i("line", line)
-                if (line != null) {
-                    result += line
-                }
-            } while (line != null)
-            inputStream.close()
-        } catch (ex: Exception) {
-
+    private fun getContent(): String? {
+        return try {
+            val request = Request.Builder()
+                .url("https://api.github.com/search/repositories?q=tiep")
+                .build()
+            client.newCall(request).execute().body?.string()
+        } catch (e: Exception) {
+            e.message
         }
-
-        return result
     }
 }
